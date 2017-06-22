058807ce5d5a2ae542623135750e8312a5720bec
Albert Nguyen <nguyen11@memphis.edu>
2017-06-22 03:43:46 -0500

    First Bytegit add .!

---
+++ AnimeBot.py
@@ -0,0 +1,21 @@
+import discord
+import asyncio
+
+from discord.ext import commands
+
+#client = discord.Client()
+client = commands.Bot(description = "AnimeFacts Bot", command_prefix = "!")
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.command(pass_context=True)
+async def hello(ctx):
+    msg = 'Hello ' + str(author.id)
+    await client.say(msg)
+
+client.run('MzI3MzU0NDY0MjcxMDczMjgw.DC0IlA.ablfsh7yqBvg-k79TXYzG1EDdc0')
