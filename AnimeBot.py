b7b690c092cb404de505298b42647c60cf94ee90
Albert Nguyen <nguyen11@memphis.edu>
2017-06-23 13:35:16 -0500

    Added Search Name

--- AnimeBot.py
+++ AnimeBot.py
@@ -1,11 +1,3 @@
-import discord
-import asyncio
-
-from discord.ext import commands
-
-#client = discord.Client()
-client = commands.Bot(description = "AnimeFacts Bot", command_prefix = "!")
-
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -17,5 +9,11 @@ async def on_ready():
 async def hello(ctx):
     msg = 'Hello World!'
     await client.say(msg)
+
+@client.command()
+async def name(*args):
+        print(args)
+        url = ("https://myanimelist.net/character.php?{}".format(urlencode({'q':' '.join(args)})))
+        return await client.say(url)
 
 client.run('MzI3MzU0NDY0MjcxMDczMjgw.DC0IlA.ablfsh7yqBvg-k79TXYzG1EDdc0')
