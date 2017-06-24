02916b801efaf565be3e5b175877980285bc413b
Albert Nguyen <nguyen11@memphis.edu>
2017-06-24 00:45:31 -0500

    Add Bot Playing Status

--- AnimeBot.py
+++ AnimeBot.py
@@ -12,10 +12,11 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
+    await client.change_presence(game=discord.Game(name='Prefix: !'))
 
 @client.command(pass_context=True)
 async def hello(ctx):
-    msg = 'Hello ' + str(author.id)
+    msg = 'Hello'
     await client.say(msg)
 
 client.run('MzI3MzU0NDY0MjcxMDczMjgw.DC0IlA.ablfsh7yqBvg-k79TXYzG1EDdc0')
